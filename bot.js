f1f8c0078e832bb9473bcda98c831e292ab221e3
YTyler <tyates907@gmail.com>
2023-01-01 18:08:27 -0800

    package install, base code

---
+++ bot.js
@@ -0,0 +1,41 @@
+const { RouteBases } = require("discord.js");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  setInterval(() => {
+    const categories = [
+      "people",
+      "nature",
+      "food",
+      "activity",
+      "travel",
+      "objects",
+    ];
+    const usedCategories = new Set();
+    const randomEmojis = [];
+
+    while (randomEmojis.length < 3) {
+      let randomCategory =
+        categories[Math.floor(Math.random() * categories.length)];
+      while (usedCategories.has(randomCategory)) {
+        randomCategory =
+          categories[Math.floor(Math.random() * categories.length)];
+      }
+      usedCategories.add(randomCategory);
+
+      const emojis = client.emojis.filter((emoji) =>
+        emoji.name.includes(randomCategory)
+      );
+      const randomIndex = Math.floor(Math.random() * emojis.size);
+      randomEmojis.push(emojis.array()[randomIndex]);
+    }
+
+    client.channels.get("YOUR_CHANNEL_ID").send(randomEmojis.join(" "));
+  }); // send message every 24 hours
+});
+
+client.login("YOUR_BOT_TOKEN");

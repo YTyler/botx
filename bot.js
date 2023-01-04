4e5e2d3e143e2b0015accad50fe0b3289e11f2c8
YTyler <tyates907@gmail.com>
2023-01-04 15:34:48 -0800

    remove interval and add comments

--- bot.js
+++ bot.js
@@ -8,36 +8,37 @@ const client = new Client({ intents: myIntents });
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  setInterval(() => {
-    const categories = [
-      "people",
-      "nature",
-      "food",
-      "activity",
-      "travel",
-      "objects",
-    ];
-    const usedCategories = new Set();
-    const randomEmojis = [];
+  //categories of emojis from discord
+  const categories = [
+    "people",
+    "nature",
+    "food",
+    "activity",
+    "travel",
+    "objects",
+  ];
+  const usedCategories = new Set();
+  const randomEmojis = [];
 
-    while (randomEmojis.length < 3) {
-      let randomCategory =
+  while (randomEmojis.length < 3) {
+    let randomCategory =
+      categories[Math.floor(Math.random() * categories.length)];
+    //reselect a category if it has been chosen already
+    while (usedCategories.has(randomCategory)) {
+      randomCategory =
         categories[Math.floor(Math.random() * categories.length)];
-      while (usedCategories.has(randomCategory)) {
-        randomCategory =
-          categories[Math.floor(Math.random() * categories.length)];
-      }
-      usedCategories.add(randomCategory);
-
-      const emojis = client.emojis.filter((emoji) =>
-        emoji.name.includes(randomCategory)
-      );
-      const randomIndex = Math.floor(Math.random() * emojis.size);
-      randomEmojis.push(emojis.array()[randomIndex]);
     }
+    usedCategories.add(randomCategory);
 
-    client.channels.get("YOUR_CHANNEL_ID").send(randomEmojis.join(" "));
-  }); // send message every 24 hours
+    //Choose an emoji from the category
+    const emojis = client.emojis.filter((emoji) =>
+      emoji.name.includes(randomCategory)
+    );
+    const randomIndex = Math.floor(Math.random() * emojis.size);
+    randomEmojis.push(emojis.array()[randomIndex]);
+  }
+  //send emoji message
+  client.channels.get("YOUR_CHANNEL_ID").send(randomEmojis.join(" "));
 });
 
 client.login("YOUR_BOT_TOKEN");

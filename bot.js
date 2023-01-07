0e65fc79c9ef2821654e78b051c08b43d83ccda7
YTyler <tyates907@gmail.com>
2023-01-06 23:07:44 -0800

    convert bot to new library

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Client, IntentsBitField } = require("discord.js");
 const CONFIG = require("./config.json");
-const data = require("emojibase-data/en/compact.json");
+const data = require("./lib-emoji.json");
 const cron = require("node-cron");
 
 const myIntents = new IntentsBitField([IntentsBitField.Flags.GuildMessages]); //bot permissions
@@ -11,31 +11,33 @@ client.login(CONFIG.BOT_TOKEN);
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
+  const EMOJI_LIBRARY = data.emojiDefinitions;
+
   cron.schedule(
     "0 0 * * *",
     () => {
-      const bannedCategories = [8, 9];
+      const bannedCategories = ["flags", "symbols"];
       const randomEmojis = [];
 
       while (randomEmojis.length < 3) {
         //Choose an emoji
-        let randomIndex = Math.floor(Math.random() * data.length);
-        let emoji = data.at(randomIndex);
+        let randomIndex = Math.floor(Math.random() * EMOJI_LIBRARY.length);
+        let emoji = EMOJI_LIBRARY.at(randomIndex);
 
         //repick if emoji from previous or banned category
-        while (bannedCategories.includes(emoji.group)) {
-          randomIndex = Math.floor(Math.random() * data.length);
-          emoji = data.at(randomIndex);
+        while (bannedCategories.includes(emoji.category)) {
+          randomIndex = Math.floor(Math.random() * EMOJI_LIBRARY.length);
+          emoji = EMOJI_LIBRARY.at(randomIndex);
         }
         //add final emoji info to arrays
-        bannedCategories.push(emoji.group);
-        randomEmojis.push(emoji.unicode);
+        bannedCategories.push(emoji.category);
+        randomEmojis.push(emoji.primaryNameWithColons);
       }
       // send emoji message
       client.channels
         .fetch(CONFIG.CHANNEL_ID)
         .then((channel) => {
-          channel.send("**Doodlebot says draw this, coward!**");
+          channel.send("**DoodleBot says draw this, coward!**");
           channel.send(randomEmojis.join(" "));
         })
         .catch(() => console.error);
